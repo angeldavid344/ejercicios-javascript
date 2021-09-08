03dba70dbee425e81ab0f3344fa587a3d01ef612
angeldavid344 <angeldavidve@gmail.com>
2021-09-08 17:18:11 -0300

    Second commit

---
+++ programa1.js
@@ -0,0 +1,52 @@
+/*
+//mi primer programa
+//suma
+var a = 3;
+var b = 13;
+var c = 15;
+
+a += 13;
+b += 17;
+c += 7;
+
+console.log(a,b,c);
+*/
+
+//resta
+/*
+var d = 11;
+var e = 3;
+var f = 17;
+
+d -= 10;
+e -= 2;
+f -= 10;
+
+console.log(d,e,f);
+*/
+
+/*
+//multiplicacion
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+console.log(a,b,c);
+*/
+
+/*
+//division
+var a = 48;
+var b = 108;
+var c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+console.log(a,b,c);
+*/
