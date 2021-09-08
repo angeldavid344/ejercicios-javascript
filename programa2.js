58bc60d1c34d17bcd5356f110f82b39447073ff0
angeldavid344 <angeldavidve@gmail.com>
2021-09-08 17:43:08 -0300

    Tercer commit

---
+++ programa2.js
@@ -0,0 +1,10 @@
+//mi segundo programa
+//agregar variables con nombres
+/*ar nombre = 'Angel';
+var apellido = 'Bravo';
+console.log(nombre,apellido)
+*/
+
+//escape de comillas en cadena
+var myStr = "Angel Bravo, \"\"\"\"\"\"Canela es linda\" y \"tiene hambre\".";
+console.log(myStr);
